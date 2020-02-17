a1ce51449eeb4ec3bd36775b2b66d79712bde4f7
Benjamin Wasty <benny.wasty@gmail.com>
2020-02-17 14:07:25 +0100

    finish https://www.django-rest-framework.org/tutorial/1-serialization/

--- src/tutorial/urls.py
+++ src/tutorial/urls.py
@@ -28,5 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('', include('snippets.urls')),
 ]
